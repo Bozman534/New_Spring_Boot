4cf933e9bbe60ec83f420d3d3d3173f6a69ffac1
roi <roi@ubuntu>
2018-08-15 10:31:57 -0700

    adding files

---
+++ src/main/java/com/acmecorp/trader/domain/ReadStockFile.java
@@ -0,0 +1,26 @@
+package com.acmecorp.trader.domain;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class ReadStockFile {
+
+	public static void readStocks (String stockName) {
+		
+		String filePath = "/home/java/java_courses/fidelity-workshop/trader/" + stockName + ".csv";
+		
+		Stream <String> stockStream = null;
+		
+		try {
+		stockStream = Files.lines(Paths.get(filePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+			}
+		
+		stockStream.forEach(System.out::println);
+		
+	}
+	
+}
---
+++ src/main/java/com/acmecorp/trader/domain/stockGenerator.java
@@ -0,0 +1,10 @@
+package com.acmecorp.trader.domain;
+
+public class stockGenerator {
+	
+	public static boolean generateStocks () {
+		
+		return true;
+	}
+
+}
