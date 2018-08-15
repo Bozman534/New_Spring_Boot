f6a87bd94ba4cfac27cecc6212c5c5cc8e5d37a3
roi <roi@ubuntu>
2018-08-15 10:17:33 -0700

    added stockReader

--- src/main/java/com/acmecorp/trader/TraderApplication.java
+++ src/main/java/com/acmecorp/trader/TraderApplication.java
@@ -2,11 +2,13 @@ package com.acmecorp.trader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.acmecorp.trader.domain.ReadStockFile;
 
 @SpringBootApplication
 public class TraderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TraderApplication.class, args);
+		ReadStockFile.readStocks("GOOG");
 	}
 }
