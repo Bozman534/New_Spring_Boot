bf169bb6d07f18f3a6a5db9c3d45538256f39c6c
roi <roi@ubuntu>
2018-08-15 13:28:18 -0700

    fixed file output

--- src/main/java/com/acmecorp/trader/domain/stockGenerator.java
+++ src/main/java/com/acmecorp/trader/domain/stockGenerator.java
@@ -6,6 +6,11 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.stream.Stream;
+
+import groovyjarjarantlr.collections.List;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -25,13 +30,37 @@ public class stockGenerator {
 			}
 		//creating the data
 		
-		Stream<String> rowContent = Stream.of("Date,Open,High,Low,Close,Volume");
+		ArrayList rowContent = new ArrayList<String>(); 
+		double open;
+		double high;
+		double low;
+		double close;
+		double volume;
+		Calendar c = Calendar.getInstance();
+		c.setTime(startDate);
+
+		rowContent.add("Date,Open,High,Low,Close,Volume");
 		
-		//For (int day)
+		for (int i = 1; i <= days; i++){
+			open = 80 * (1.1 * Math.random());
+			high = 85 * (1.1 * Math.random());
+			low = 75 * (1.1 * Math.random());
+			close = 80 * (1.1 * Math.random());
+			volume = 2000000 * (1.2 * Math.random());
+			
+			rowContent.add(startDate.toString() + "," + open + "," + high + "," + low + "," + close + "," + volume);
+			c.add(Calendar.DAY_OF_MONTH, 1);
+			startDate = c.getTime();
+			
+		}
 
 		
 		try {
-		Files.write(filePath, rowContent.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+	
+		Files.write(filePath, rowContent, StandardOpenOption.APPEND);
+		
+		//.getBytes(StandardCharsets.UTF_8)
+		
 		} catch (IOException e) {
 			e.printStackTrace();
 			}		
